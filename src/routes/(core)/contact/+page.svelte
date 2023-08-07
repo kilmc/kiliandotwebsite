d9995f7f96bdf8a5f6b946566e02cf55ab438b20
Kilian McMahon <kilmc@fastmail.com>
2023-08-07 13:20:01 +0200

    fix: add info to contact page

--- src/routes/(core)/contact/+page.svelte
+++ src/routes/(core)/contact/+page.svelte
@@ -1,1 +1,6 @@
-Contact
+<div class="flex flex-col items-start">
+	<a class="link" href="mailto:mail@kilian.website">Email</a>
+	<a class="link" href="https://xoxo.zone/@kilmc">Mastodon</a>
+	<a class="link" href="https://instagram.com/kilmc">Instagram</a>
+	<a class="link" href="https://twitter.com/kilmc">Twitter (Dorment)</a>
+</div>
