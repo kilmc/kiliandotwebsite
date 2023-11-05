f42b7a1e9415d5143d9004767f89757e8c95fb66
Kilian McMahon <kilmc@fastmail.com>
2023-11-05 10:28:49 +0100

    feat: remove duplicate keys and move computation the server

--- src/routes/(core)/tools/music/keys/+page.svelte
+++ src/routes/(core)/tools/music/keys/+page.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
-	import { majorModalKeyNames } from '@kilmc/music-fns';
-	import { convertKeyToURL } from '$lib/components/music/helpers';
+	import type { PageData } from './$types';
 
-	const scaleGroups = Object.entries(majorModalKeyNames);
-
-	const romanNumeralClasses = [
-		'bg-red-100 dark:bg-red-200 dark:text-red-900',
-		'bg-orange-100 dark:bg-orange-200 dark:text-orange-900',
-		'bg-yellow-100 dark:bg-yellow-200 dark:text-yellow-900',
-		'bg-green-100 dark:bg-green-200 dark:text-green-900',
-		'bg-blue-100 dark:bg-blue-200 dark:text-blue-900',
-		'bg-indigo-100 dark:bg-indigo-200 dark:text-indigo-900',
-		'bg-violet-100 dark:bg-violet-200 dark:text-violet-900'
-	];
+	export let data: PageData;
 </script>
 
-{#each scaleGroups as [name, scaleNames], index}
+{#each data.majorScaleGroups as [name, scaleInfo], index}
 	<div class="mb-10">
 		<h2 class="font-bold text-xl mb-2">{name.toUpperCase()}</h2>
 		<div class="flex flex-wrap gap-x-2 gap-y-1">
-			{#each scaleNames as scaleName}
-				<a
-					href={convertKeyToURL(scaleName)}
-					class="{romanNumeralClasses[index]} px-2 rounded-md capitalize"
-				>
-					{scaleName}
+			{#each scaleInfo as { name, href, classes }}
+				<a {href} class="{classes} px-2 rounded-md capitalize">
+					{name}
 				</a>
 			{/each}
 		</div>
