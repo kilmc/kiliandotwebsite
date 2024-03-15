b29e9ec82d804dd7451e85b1aa24bb3dc48851bc
Kilian McMahon <kilmc@fastmail.com>
2024-03-15 14:24:49 +0100

    feat: add initial page for hire-me

---
+++ src/routes/(blank)/work-with-me/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let title = '';
+	export let year = '';
+</script>
+
+<div class="text-2xl bg-white rounded-lg shadow-md px-8 py-8">
+	<div>{title}</div>
+	<div class="text-base mb-4">{year}</div>
+	<p class="text-base">
+		<slot />
+	</p>
+</div>
